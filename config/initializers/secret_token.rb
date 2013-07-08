98534862cfeb6a541563dca9048d11e7903711d9
Margaret Scott <eudaimonious@gmail.com>
2013-07-08 05:16:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DumplingHouse::Application.config.secret_token = '3d70c13e01dced001d52ca1847cda0572a7c27dad8f05d6fa84d2c8ed573806b22fd65d2257fe35853e999ba395e95a34e8104e1bf6ea59b35791d96694980f4'
